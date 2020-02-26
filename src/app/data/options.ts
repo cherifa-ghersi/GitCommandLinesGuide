9af47901555481ede5aa8a9fee503a891aa0d85d
cherifa ghersi <cherifa.ghersi-sc@alehos.Fr>
2020-02-26 15:16:19 +0100

    add all problem oh shit git

--- src/app/data/options.ts
+++ src/app/data/options.ts
@@ -8,18 +8,43 @@ export const problems : Problem[] = [
   },
   {
     id: 2,
-    value: 'California',
+    value: 'I committed and immediately realized I need to make one small change!',
     solution : 'git reflog'
 
   },
   {
     id: 3,
-    value: 'Florida',
+    value: 'I need to change the message on my last commit!',
     solution : 'git reflog'
   },
   {
     id: 4,
-    value: 'Texas',
+    value: ' accidentally committed something to master that should have been on a brand new branch!',
+    solution : 'git reflog'
+  },
+  {
+    id: 5,
+    value: 'I accidentally committed to the wrong branch!',
+    solution : 'git reflog'
+  },
+  {
+    id: 6,
+    value: 'I tried to run a diff but nothing happened?!',
+    solution : 'git reflog'
+  },
+  {
+    id: 7,
+    value: 'I need to undo a commit from like 5 commits ago!',
+    solution : 'git reflog'
+  },
+  {
+    id: 8,
+    value: 'I need to undo my changes to a file!',
+    solution : 'git reflog'
+  },
+  {
+    id: 9,
+    value: 'I need to undo my changes to a file!',
     solution : 'git reflog'
   }
 ];
